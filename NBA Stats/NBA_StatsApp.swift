5161d4422aaf162fe4424f5ff63fdc4684fc17e0
Hung-Chun, Tsai <tsaihong1995@gmail.com>
2021-05-12 10:20:14 -0400

    Initial Commit

---
+++ NBA Stats/NBA_StatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBA_StatsApp.swift
+//  NBA Stats
+//
+//  Created by Hung-Chun Tsai on 2021-05-12.
+//
+
+import SwiftUI
+
+@main
+struct NBA_StatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
